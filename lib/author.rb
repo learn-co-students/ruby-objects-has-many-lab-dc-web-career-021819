a9676168538abc34f476ceb5748cc4834746e7da
Ammar Ali <funnyguy118@gmail.com>
2019-02-21 18:39:43 -0500

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,26 @@
+class Author
+
+  attr_accessor :name, :posts
+  @@all_posts = []
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def add_post (post)
+    post.author = self
+    @posts << post
+    @@all_posts << post
+  end
+
+  def add_post_by_title(post_title)
+    post = Post.new(post_title)
+    self.add_post(post)
+  end
+
+  def self.post_count
+    @@all_posts.length
+  end
+
+end
